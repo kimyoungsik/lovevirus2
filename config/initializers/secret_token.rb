117f61c04c200e95c572f81933c2ca0f6bc83539
Kim Youngsik <dean4208@gmail.com>
2013-07-17 02:33:00 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lovevirus::Application.config.secret_key_base = 'db476470a2ffdd959117793cbf3d7aa8f2c854cfe476ce69862833a885db8ab441734f0ef839cf1e36f168b67446b44c5fb600b4009b4eb1c0ad7bede0a391f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lovevirus::Application.config.session_store :cookie_store, key: '_lovevirus_session'
